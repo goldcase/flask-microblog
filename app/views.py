afc06226617f72530e62164d61fbdcd402ea447e
Johnny Chang <johnny@johnnychang.com>
2015-07-27 22:42:50 -0500

    added mock posts and iterator through posts array

--- app/views.py
+++ app/views.py
@@ -4,7 +4,21 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    # Fake user
+    # Mock user
     user = {'nickname': 'Johnny'}
-    # render template with binding to title and user
-    return render_template('index.html', title='Home', user=user)
+    # Mock posts
+    posts = [
+    	{
+    		'author': {'nickname': 'Babel'},
+    		'body': 'Working at kCura'
+    	},
+    	{
+    		'author': {'nickname': 'Dave'},
+    		'body': 'Jim Cramer is Wrong'
+    	}
+    ]
+    # Render template with binding to title and user
+    return render_template('index.html', 
+    						title='Home', 
+    						user=user,
+    						posts=posts)
